0ffb43380b407c4063ac0f81f79b52d88ce4741c
Fh-Ndiritu <fhndiritu@gmail.com>
2023-05-19 21:13:34 +0300

    Dominator solution with O(n) complexicity

--- lesson_8/Dominator.rb
+++ lesson_8/Dominator.rb
@@ -1,5 +1,3 @@
-
-
 def solution(a)
     #O(n)
     return 0 if a.size == 1
@@ -19,19 +17,7 @@ def solution(a)
 
 end
 
-def solution(a)
-    #O(nlogn && n/2)  ===> #O(nlogn)
-    tr = a.tally #O(n)
-    tr_max = tr.sort_by{|k, v| v}.last #O(logn)
 
-    unless a.empty? 
-        return a.find_index(tr_max.first)   if tr_max.last > a.size/2 #O(n/2)
-    end
-
-
-    return -1
-
-end
 
 
 p solution([-1])
